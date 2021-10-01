988d53547805e11d32b78f74785f03b6d1f6af1a
st <shinthanttun.dev@gmail.com>
2021-10-01 09:04:07 +0630

    urls config

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('about'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path("",views.AboutView.as_view(),name='about')
 ]
