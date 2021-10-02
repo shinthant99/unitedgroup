259c42925aa5e1b54493740ec27c365ab7ddb1af
st <shinthanttun.dev@gmail.com>
2021-10-02 21:04:41 +0630

    refresh still unresolved url

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('about/', views.AboutView.as_view(), name='about'),
-    path("",views.AboutView.as_view(),name='about')
+    path("",views.AboutView.as_view(),name='index'),
+    path("contact", views.ContactView.as_view(), name='contact'),
+    path("index", views.IndexView.as_view(), name="index")
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,3 +3,9 @@ from django.views.generic import TemplateView
 # Create your views here.
 class AboutView(TemplateView):
     template_name = 'about.html'
+
+class ContactView(TemplateView):
+    template_name = 'contact.html'
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
