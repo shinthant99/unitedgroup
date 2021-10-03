78943441c2991a03af317f1b197693b954c2c546
st <shinthanttun.dev@gmail.com>
2021-10-03 21:27:56 +0630

    template fixes

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,7 +3,13 @@ from . import views
 
 urlpatterns = [
     path('about/', views.AboutView.as_view(), name='about'),
-    path("",views.AboutView.as_view(),name='index'),
-    path("contact", views.ContactView.as_view(), name='contact'),
-    path("index", views.IndexView.as_view(), name="index")
+    path("",views.IndexView.as_view(),name='index'),
+    path("contact/", views.ContactView.as_view(), name='contact'),
+    path("index/", views.IndexView.as_view(), name="index"),
+    path("logistics/", views.LogisticsView.as_view(), name="logistics"),
+    path("newyangonlionsclub/", views.NewYangonLionsClubView.as_view(), name="newyangonlionsclub"),
+    path("trading/", views.TradingView.as_view(), name="trading"),
+    path("internationalremittance/", views.WakhemaInternationalRemittanceView.as_view(), name="wakhemainternationalremittance"),
+    path("changer/", views.WakhemaMoneyChangerView.as_view(), name="wakhemamoneychanger"),
+    path("yse/", views.YouthSocietyForEducationView.as_view(), name="yse"),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -9,3 +9,21 @@ class ContactView(TemplateView):
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class LogisticsView(TemplateView):
+    template_name = 'logistics.html'
+
+class NewYangonLionsClubView(TemplateView):
+    template_name = 'new yangon lions.html'
+
+class TradingView(TemplateView):
+    template_name = 'trading.html'
+
+class WakhemaInternationalRemittanceView(TemplateView):
+    template_name = 'wakhema international remittance.html'
+
+class WakhemaMoneyChangerView(TemplateView):
+    template_name = 'wakhema money changer.html'
+
+class YouthSocietyForEducationView(TemplateView):
+    template_name = 'youth society for education.html'
